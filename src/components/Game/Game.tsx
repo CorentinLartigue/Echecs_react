77bd70e2d461640252327f7931eb2793c1ee7b6d
Boidin Yanis <yanis.boidin@guarani.fr>
2024-11-12 10:31:27 +0100

    DebutDuProjet

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,59 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Title from 'src/components/Shared/Title';
-import ChessBoard from 'src/components/Game/ChessBoard/ChessBoard';
-import Grid from 'src/components/Game/Grid/Grid.tsx';
-
+import React, {} from 'react';
+import Title from '../Shared/Title';
 
 const Game: React.FC = () => {
 
-
-  /* Fonction pour générer l'échiquier initial */
-  const generateInitialBoard = () => {
-    return [
-      ['♖', '♘', '♗', '♔', '♕', '♗', '♘', '♖'],
-      ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
-      ['♜', '♞', '♝', '♔', '♚', '♝', '♞', '♜'],
-    ];
-  };
-
-  return (
+return(
     <div>
-      <Title text="Échecs" />
-    
+        <Title text="rejgergierger"/>
     </div>
-  );
+);
 };
 
 export default Game;
-
-
-
-/*Fonction pour choisir sa couleur en donnant son nom Le blanc commence toujours en premier*/
-
-/* Fonction pour basculer le tour
-const switchTurn = () => {
-  setTurn(turn === 'white' ? 'black' : 'white');
-};*/
-
-// Fonction pour gérer le mouvement des pièce
-
-
-/* Fonction pour générer l'échiquier initial
-const generateInitialBoard = (): Board => {
-  return [
-    ['♖', '♘', '♗', '♔', '♕', '♗', '♘', '♖'],
-    ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
-    ['♜', '♞', '♝', '♔', '♚', '♝', '♞', '♜'],
-  ];
-};*/
-
